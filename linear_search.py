f3c6251300ea56f3a3161f8c85f495823651ae9b
MukunthanPeriyasamy <mukunthanperiyasamy04@gmil.com>
2025-01-12 08:07:32 +0530

    added Linear_search file

---
+++ linear_search.py
@@ -0,0 +1,10 @@
+def linear_search(arr,target,index):
+    if index == len(arr) -1:
+        return f"Element not found"
+    elif arr[index] == target:
+        return f"The Element found at index {index}"
+    return linear_search(arr,target,index+1)
+arr = [3,4,5,1,6,7,8,0]
+target = 9
+result = linear_search(arr,target,0)
+print(result)
