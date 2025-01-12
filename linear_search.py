3e256b31003e02a51fce6f2f89ae231d25ba9dbc
MukunthanPeriyasamy <mukunthanperiyasamy04@gmil.com>
2025-01-12 08:10:29 +0530

    changed file

--- linear_search.py
+++ linear_search.py
@@ -5,6 +5,6 @@ def linear_search(arr,target,index):
         return f"The Element found at index {index}"
     return linear_search(arr,target,index+1)
 arr = [3,4,5,1,6,7,8,0]
-target = 9
+target = 6
 result = linear_search(arr,target,0)
 print(result)
